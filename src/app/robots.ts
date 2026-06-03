71823b997123e463781138c2b5fedbf8a1f7c090
Ike lloyd Yana <ikeyana@Ikes-MacBook-Pro.local>
2026-06-03 21:00:05 +0800

    fix robot

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,29 +1,13 @@
 import { MetadataRoute } from 'next'
 import { SITE_URL } from '@/lib/site'
 
-const AI_CRAWLERS = [
-  'Amazonbot',
-  'Applebot-Extended',
-  'Bytespider',
-  'CCBot',
-  'ClaudeBot',
-  'GPTBot',
-  'meta-externalagent',
-] as const
-
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: '*',
-        allow: '/',
-        disallow: ['/dashboard/', '/admin/', '/checkout', '/api/'],
-      },
-      ...AI_CRAWLERS.map((userAgent) => ({
-        userAgent,
-        disallow: '/',
-      })),
-    ],
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/dashboard/', '/admin/', '/checkout', '/api/'],
+    },
     sitemap: `${SITE_URL}/sitemap.xml`,
   }
 }
