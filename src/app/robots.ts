e7281ec861c13580481e37129553f5b726ce1e7d
Ike lloyd Yana <ikeyana@Ikes-MacBook-Pro.local>
2026-06-03 20:56:15 +0800

    fix robot

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,22 +1,29 @@
 import { MetadataRoute } from 'next'
 import { SITE_URL } from '@/lib/site'
 
-function canonicalHost(): string {
-  try {
-    return new URL(SITE_URL).hostname
-  } catch {
-    return 'calorieapi.com'
-  }
-}
+const AI_CRAWLERS = [
+  'Amazonbot',
+  'Applebot-Extended',
+  'Bytespider',
+  'CCBot',
+  'ClaudeBot',
+  'GPTBot',
+  'meta-externalagent',
+] as const
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/dashboard/', '/admin/', '/checkout', '/api/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/dashboard/', '/admin/', '/checkout', '/api/'],
+      },
+      ...AI_CRAWLERS.map((userAgent) => ({
+        userAgent,
+        disallow: '/',
+      })),
+    ],
     sitemap: `${SITE_URL}/sitemap.xml`,
-    host: canonicalHost(),
   }
 }
